3eb97af13003537a04b79988a0cd45287e300b3a
Fábio Vieira Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2016-10-25 13:52:07 +0100

    Implemented Right Priority List

--- Assets/Scripts/IAJ.Unity/Pathfinding/DataStructures/RightPriorityList.cs
+++ Assets/Scripts/IAJ.Unity/Pathfinding/DataStructures/RightPriorityList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Assets.Scripts.IAJ.Unity.Pathfinding.DataStructures
 {
@@ -9,71 +10,69 @@ namespace Assets.Scripts.IAJ.Unity.Pathfinding.DataStructures
 
         public RightPriorityList()
         {
-            this.Open = new List<NodeRecord>();    
+            this.Open = new List<NodeRecord>();
+            //this.Open.Sort((n1, n2) => n1.fValue >= n2.fValue ? -1 : 1);
         }
         public void Initialize()
         {
-            //TODO implement
-            throw new NotImplementedException();
+            Open.Clear();
         }
 
         public void Replace(NodeRecord nodeToBeReplaced, NodeRecord nodeToReplace)
         {
-            //TODO implement
-            throw new NotImplementedException();
+            Open.Remove(nodeToBeReplaced);
+            AddToOpen(nodeToReplace);
         }
 
         public NodeRecord GetBestAndRemove()
         {
-            //TODO implement
-            throw new NotImplementedException();
+            var best = PeekBest();
+            this.Open.Remove(best);
+            return best;
         }
 
         public NodeRecord PeekBest()
         {
-            //TODO implement
-            throw new NotImplementedException();
+            return Open[Open.Count - 1];
         }
 
         public void AddToOpen(NodeRecord nodeRecord)
         {
             //a little help here, notice the difference between this method and the one for the LeftPriority list
             //...this one uses a different comparer with an explicit compare function (which you will have to define below)
-            int index = this.Open.BinarySearch(nodeRecord,this);
+            int index = this.Open.BinarySearch(nodeRecord, this);
             if (index < 0)
             {
+                //Debug.Log("add to open: " + (~index >= this.Open.Count ? 0 : this.Open[~index].fValue) + " -> " + nodeRecord.fValue);
                 this.Open.Insert(~index, nodeRecord);
             }
         }
 
         public void RemoveFromOpen(NodeRecord nodeRecord)
         {
-            //TODO implement
-            throw new NotImplementedException();
+            this.Open.Remove(nodeRecord);
         }
 
         public NodeRecord SearchInOpen(NodeRecord nodeRecord)
         {
-            //TODO implement
-            throw new NotImplementedException();
+            return this.Open.Find(n => n.Equals(nodeRecord));
         }
 
         public ICollection<NodeRecord> All()
         {
-            //TODO implement
-            throw new NotImplementedException();
+            return this.Open;
         }
 
         public int CountOpen()
         {
-            //TODO implement
-            throw new NotImplementedException();
+            return this.Open.Count;
         }
 
         public int Compare(NodeRecord x, NodeRecord y)
         {
-            //TODO implement
-            throw new NotImplementedException();
+            if (x.fValue == y.fValue) return 0;
+            else if (x.fValue < y.fValue) return 1;
+            else return -1;
         }
     }
 }
